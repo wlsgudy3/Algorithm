929d820ad44d6b9d3694ae885d6744dea49017d4
wlsgudy3 <wlsgudy3@naver.com>
2023-01-15 17:34:20 +0900

    practice

--- Keundol/week_1/practice.cpp
+++ Keundol/week_1/practice.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int arr[100001], psum[100001], n;
+
 int main() {
-    cout << "practice.cpp";
-    return 0;
+	int max, min;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+		if (arr[i] == 1) {
+			psum[i] = psum[i - 1] + 1;
+		}
+		else if (arr[i] == 2) {
+			psum[i] = psum[i - 1] - 1;
+		}
+	}
+
+	max = *max_element(psum, psum + n + 1);
+	min = *min_element(psum, psum + n + 1);
+
+	cout << max - min;
 }
