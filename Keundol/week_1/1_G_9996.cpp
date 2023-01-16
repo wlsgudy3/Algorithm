301d3f2ecb881ebc724d34430c3ec30042d82a5f
wlsgudy3 <wlsgudy3@naver.com>
2023-01-16 17:35:00 +0900

    after

--- Keundol/week_1/1_G_9996.cpp
+++ Keundol/week_1/1_G_9996.cpp
@@ -1,41 +1,26 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
 
 using namespace std;
 
-string str, pattern, a, b, temp;
-int n, star;
+int n, pos;
+string pattern, str, pre, suf;
 
 int main() {
 	cin >> n;
 	cin >> pattern;
-	for (int i = 0; i < pattern.size(); i++) {
-		if (pattern[i] == '*') {
-			star = i;
-			break;
-		}
-	}
-	a = pattern.substr(0, star);
-	b = pattern.substr(star + 1);
+	pos = pattern.find('*');
+
+	pre = pattern.substr(0, pos);
+	suf = pattern.substr(pos + 1);
 
 	for (int i = 0; i < n; i++) {
 		cin >> str;
-		temp = str.substr(0, a.size());
-		if (str.size() < a.size() + b.size()) {
-			cout << "NE" << "\n";
-			continue;
+		if (pre.size() + suf.size() > str.size())
+			cout << "NE\n";
+		else {
+			if (pre == str.substr(0, pre.size()) && suf == str.substr(str.size() - suf.size())) cout << "DA\n";
+			else cout << "NE\n";
 		}
-		if (temp != a) { 
-			cout << "NE" << "\n";
-			continue;
-		}
-		temp = str.substr(str.size() - b.size());
-		if (temp != b) {
-			cout << "NE" << "\n";
-			continue;
-		}
-		cout << "DA" << "\n";
 	}
 	return 0;
 }
