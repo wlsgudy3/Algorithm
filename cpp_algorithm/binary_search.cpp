987fd5486e029f3a304daab8ec806709718d3c64
wlsgudy3 <wlsgudy3@naver.com>
2023-02-07 13:44:04 +0900

    binary_search_recursive

--- cpp_algorithm/binary_search.cpp
+++ cpp_algorithm/binary_search.cpp
@@ -1,4 +1,4 @@
-
+//반복문
 int binarySearch(int arr[], int size, int key) {
 	int low = 0; high = size - 1;
 
@@ -14,4 +14,18 @@ int binarySearch(int arr[], int size, int key) {
 	}
 	
 	return -1;
+}
+
+//재귀호출
+int binarySearch(int arr[], int low, int high, int key) {
+	if (low > high) return -1;
+
+	int mid = (low + high) / 2;
+
+	if (arr[mid] == key)
+		return mid;
+	else if (arr[mid] > key)
+		return binarySearch(arr, low, mid - 1, key);
+	else
+		return binarySearch(arr, mid + 1, high, key);
 }
